4731f00df8394576ee0cccbe5dce46013c915a45
Devepre <bicycle2life@gmail.com>
2017-12-05 13:27:48 +0200

    initial commit

---
+++ hw01_01/src/main/java/com/gmail/bicycle/App.java
@@ -0,0 +1,13 @@
+package com.gmail.bicycle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
