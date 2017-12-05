3f4d41fc4847317cd2acbfddc6d22fc5793b22f0
Devepre <bicycle2life@gmail.com>
2017-12-05 21:41:54 +0200

    executable demo of using this library

--- hw01_01/src/main/java/com/gmail/bicycle/App.java
+++ hw01_01/src/main/java/com/gmail/bicycle/App.java
@@ -1,13 +1,32 @@
 package com.gmail.bicycle;
 
+import java.util.List;
+
+import com.gmail.bicycle.annotation.ParametersInvoker;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("<Demo using of current library>");
+
+		AnnotatedClass annotedObject = new AnnotatedClass();
+		ParametersInvoker parametersInvoker = new ParametersInvoker(annotedObject, ComputeValues.class);
+		List<Object> results = parametersInvoker.getResults();
+
+		printResults(results);
+	}
+
+	public static void printResults(List<Object> results) {
+		if (results!= null) {
+			System.out.println("Results of annotated methods are:");
+			results.stream()
+				.forEach(System.out::println);
+		} else {
+			System.out.println("Empty results!");
+		}
+		
+	}
 }
