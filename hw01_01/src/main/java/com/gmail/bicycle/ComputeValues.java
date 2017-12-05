d8cb977742160a729f94ebb139c9ea2200c37f4e
Devepre <bicycle2life@gmail.com>
2017-12-05 21:37:15 +0200

    add base annotation version

---
+++ hw01_01/src/main/java/com/gmail/bicycle/ComputeValues.java
@@ -0,0 +1,14 @@
+package com.gmail.bicycle;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(value = ElementType.METHOD)
+public @interface ComputeValues {
+	int paramOne();
+	int paramTwo();
+
+}
