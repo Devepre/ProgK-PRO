65444afe955f792a87a90e39fbd8346b53cbe500
Devepre <bicycle2life@gmail.com>
2017-12-07 14:35:56 +0200

    demo using example

--- hw01_02/src/main/java/com/gmail/bicycle/App.java
+++ hw01_02/src/main/java/com/gmail/bicycle/App.java
@@ -1,13 +1,19 @@
 package com.gmail.bicycle;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import com.gmail.bicycle.api.SaverWrapper;
+import com.gmail.bicycle.api.TextContainer;
+
+public class App {
+	public static void main(String[] args) {
+		if (args.length > 0 && args[0].getClass().equals(String.class)) {
+			String info = args[0];
+			TextContainer textContainer = new TextContainer(info);
+			SaverWrapper worker = new SaverWrapper();
+			worker.run(textContainer);
+			System.out.println("Done.");
+		} else {
+			System.out.println("Usage: App <\"string information\">");
+		}
+	}
+
 }
