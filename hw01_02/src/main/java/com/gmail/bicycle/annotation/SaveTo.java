83586b7413bea4f39a1bbed1f19337edc4a3bc00
Devepre <bicycle2life@gmail.com>
2017-12-07 14:34:10 +0200

    implementation of Annotations

---
+++ hw01_02/src/main/java/com/gmail/bicycle/annotation/SaveTo.java
@@ -0,0 +1,13 @@
+package com.gmail.bicycle.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface SaveTo {
+	public String path() default "data.d";
+}
