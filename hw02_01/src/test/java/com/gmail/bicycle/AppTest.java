b73c65e0dab82da12be90568dfb6b1aea9cac30b
Devepre <bicycle2life@gmail.com>
2017-12-09 21:45:56 +0200

    initilize hw02_01

---
+++ hw02_01/src/test/java/com/gmail/bicycle/AppTest.java
@@ -0,0 +1,16 @@
+package com.gmail.bicycle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	@Disabled
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
